aca95f87d17fa6fcec143ad3819772179a4dd248
JesacaLin <JesacaLin@gmail.com>
2023-11-21 21:56:53 -0500

    Add GUI

--- src/App.js
+++ src/App.js
@@ -1,14 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  const value = "World";
-  //create another variable called hope and assigned value of string "I hope I can do this"
-  const hope = "I hope I can do this";
-  //create another variable called wow and assigned it the value of "wow, did I find a loophole?"
-  const wow = "wow, did I find a loophole?";
+  const title = "Computer Vision";
+  const [value, setValue] = React.useState("");
+
+  //create a state variable to store the URL
+  const [url, setUrl] = React.useState("");
+  //create a state variable to store the image analysis results
+  const [imageAnalysis, setImageAnalysis] = React.useState("");
+  //create a state variable to store the text prompt
+  const [textPrompt, setTextPrompt] = React.useState("");
+
+  //declare a function to process the URL
+  // const processUrl = () => {
+  //   //call the API to process the URL
+  //   //store the results in the state variable
+  // };
+
   return (
     <div>
-      Hello {value}, {hope}! {wow}
+      <h1>{title}</h1>
+      <h4>Insert URL or type prompt:</h4>
+      <input
+        type="text"
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Enter URL to analyze or textual prompt to generate an image"
+        style={{
+          width: "500px",
+          height: "30px",
+          borderRadius: "8px",
+          border: "1px solid #000",
+          padding: "4px",
+        }}
+      />
+      <br />
+      <button onClick={() => console.log("Analyze")}>Analyze</button>
+      <button onClick={() => console.log("Generate")}>Generate</button>
     </div>
   );
 }

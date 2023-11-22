264b3665b531f8705b49d18472e0edd6af4d6197
JesacaLin <JesacaLin@gmail.com>
2023-11-21 21:16:43 -0500

    Add another variable

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   const value = "World";
   //create another variable called hope and assigned value of string "I hope I can do this"
   const hope = "I hope I can do this";
+  //create another variable called wow and assigned it the value of "wow, did I find a loophole?"
+  const wow = "wow, did I find a loophole?";
   return (
     <div>
       Hello {value}, {hope}

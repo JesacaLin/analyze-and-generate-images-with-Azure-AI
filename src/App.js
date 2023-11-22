7e247960ae9ee56b79d122de17f2dc6aa248063b
JesacaLin <JesacaLin@gmail.com>
2023-11-21 21:13:04 -0500

    Change message

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const value = "World";
+  //create another variable called hope and assigned value of string "I hope I can do this"
+  const hope = "I hope I can do this";
+  return (
+    <div>
+      Hello {value}, {hope}
+    </div>
+  );
 }
 
 export default App;
